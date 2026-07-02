04c837ffdea1c3b54190f979781d35e86cc51f41
Justin Andres Badilla Ramirez <justinbadilla94@gmail.com>
2026-07-01 19:42:41 -0600

    Enhance Nodo class with getters and setters
    Added getter and setter methods for 'dato' and 'siguiente'.

--- Nodo.java
+++ Nodo.java
@@ -4,18 +4,29 @@
  */
 package rpncalculadora;
 
-/**
- *
- * @author justi
- */
 public class Nodo<T> {
- 
+
     T dato;
     Nodo<T> siguiente;
- 
+
     public Nodo(T dato) {
         this.dato = dato;
         this.siguiente = null;
     }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
 }
- 
